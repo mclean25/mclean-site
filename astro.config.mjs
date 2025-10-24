ea6a0fefe12a5b052c358e2ebd7d771a091a9eea
Alex McLean <alex.mclean25@gmail.com>
2025-10-23 21:54:01 -0700

    Adding cloudflare packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,15 +5,21 @@ import tailwindcss from "@tailwindcss/vite";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   devToolbar: {
     enabled: false,
   },
+
   redirects: {
     "/": "/about",
   },
+
   vite: {
     plugins: [tailwindcss(), mdx(), sitemap()],
   },
+
+  adapter: cloudflare(),
 });
